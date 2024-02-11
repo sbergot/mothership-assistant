3a135df6c325e6214563eeea52c96300c38261ad
Preston M. Firestone <firestone.preston@gmail.com>
2024-02-11 02:19:20 -0600

    src/UI/Organisms/CharacterShort.tsx: DmScreen now shows skills.
    Each player character shows a summary of their skills in the summarized
    character sheet in the left bar.

--- src/UI/Organisms/CharacterShort.tsx
+++ src/UI/Organisms/CharacterShort.tsx
@@ -1,7 +1,8 @@
 import { allSaves, allStats } from "Rules/data";
 import { Character, Contractor } from "Rules/types";
 import { TrashIcon } from "UI/Icons";
-import { Rating, Gauge } from "UI/Molecules";
+import { Rating, Gauge, Skill } from "UI/Molecules";
+import { allSkillsDict } from "Rules/Data/skills";
 
 interface Props {
   character: Character;
@@ -12,43 +13,52 @@ export function CharacterShort({ character, onTitleClick }: Props) {
   return (
     <div className="rounded-xl bg-mother-2 flex flex-col gap-4">
       <div
-        className="rounded-3xl bg-mother-6 text-mother-1 text-center cursor-pointer hover:bg-mother-5"
-        onClick={onTitleClick}
+	className="rounded-3xl bg-mother-6 text-mother-1 text-center cursor-pointer hover:bg-mother-5"
+	onClick={onTitleClick}
       >
-        {character.name}
+	{character.name}
       </div>
       <div className="flex flex-col gap-2 p-4">
-        <div className="flex flex-wrap justify-around gap-x-4 self-center">
-          {allStats.map((s) => (
-            <Rating key={s} title={s} value={character[s]} />
-          ))}
-        </div>
-        <div className="flex justify-center gap-8">
-          {allSaves.map((s) => (
-            <Rating key={s} title={s} value={character[s]} />
-          ))}
-        </div>
-        <div className="flex flex-wrap justify-around">
-          <Gauge
-            title="Health"
-            limitName="Maximum"
-            current={character.health}
-            limit={character.maxHealth}
-          />
-          <Gauge
-            title="Wounds"
-            limitName="Maximum"
-            current={character.wounds}
-            limit={character.maxWounds}
-          />
-          <Gauge
-            title="Stress"
-            limitName="Minimum"
-            current={character.stress}
-            limit={character.minStress}
-          />
-        </div>
+	<div className="flex flex-wrap justify-around gap-x-4 self-center">
+	  {allStats.map((s) => (
+	    <Rating key={s} title={s} value={character[s]} />
+	  ))}
+	</div>
+	<div className="flex justify-center gap-8">
+	  {allSaves.map((s) => (
+	    <Rating key={s} title={s} value={character[s]} />
+	  ))}
+	</div>
+	<div className="flex flex-wrap justify-around">
+	  <Gauge
+	    title="Health"
+	    limitName="Maximum"
+	    current={character.health}
+	    limit={character.maxHealth}
+	  />
+	  <Gauge
+	    title="Wounds"
+	    limitName="Maximum"
+	    current={character.wounds}
+	    limit={character.maxWounds}
+	  />
+	  <Gauge
+	    title="Stress"
+	    limitName="Minimum"
+	    current={character.stress}
+	    limit={character.minStress}
+	  />
+	</div>
+	<div className="flex flex-wrap justify-center items-center gap-4">
+	  {character.skills.map((s) => (
+	      <Skill
+		key={s.type}
+		skill={allSkillsDict[s.type]}
+		onClick={() => {}}
+		/>
+	  ))}
       </div>
     </div>
-  );
+  </div>
+);
 }
