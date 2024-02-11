15dbe6e27f1b08f7b0ecab133b95af1b73e41447
Preston M. Firestone <firestone.preston@gmail.com>
2024-02-11 01:22:53 -0600

    Enable in-source tests.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -16,5 +17,8 @@ export default defineConfig({
         "debug-icons.html",
       ],
     },
+  },
+  test: {
+      includeSource: ["src/**/*.ts"],
   },
 });
